6a78b6295fce2380618b6372a83ef30a2375ccd0
Gemma Brown <bayleygemm@gmail.com>
2018-08-11 17:02:39 +1200

    Added all things

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :games
+  post "/games/:id/make_bet", to: "games#make_bet"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
